139eb3da4bb29f10913e7114f6828b02f9f2a138
novinary <novina.dev@gmail.com>
2019-02-21 08:20:06 +0000

    More comments for future ref

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Character from './Character'
 
+// components accept arbitary inputes called props and return React
+//elemnt descrbing what should appear on the function 
 const CharacterCard = props => {
     return(
         <div>
-            {props.data.map(char => 
+            {props.data.map(char =>   //Rendering an Array of Data with map
                 <Character characterData={char} />
                 )}
         </div>
